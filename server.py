3d11fdd3a7baf58978ce1bf2ad323ee8afafb127
mulefish <finch.wren.swift@gmail.com>
2020-04-07 13:00:39 -0700

    changing machines

--- server.py
+++ server.py
@@ -1,4 +1,26 @@
 from aiohttp import web
+import time
+
+tokenHolder = {}
+tokenHolder['token'] = None
+tokenHolder['when'] = None
+
+async def fetch(session, url):
+    async with session.get(url) as response:
+        return await response.text()
+
+async def setToken():
+    global tokenHolder
+    rightNow = time.time()
+    if rightNow - tokenHolder['when'] > 60:
+        async with aiohttp.ClientSession() as session:
+            token = await fetch(session, 'http://localhost:5000/jwttoken')
+            tokenHolder['when'] = rightNow
+            tokenHolder['token'] = token
+
+
+async def exec(request):
+    return web.Response(text='hello world')
 
 async def handle(request):
     name = request.match_info.get('name', "Anonymous")
@@ -7,7 +29,8 @@ async def handle(request):
     return web.Response(text=text)
 
 app = web.Application()
-app.add_routes([web.get('/', handle),
+app.add_routes([web.get('/exec', exec), 
+                web.get('/', handle),
                 web.get('/{name}', handle)])
 
 if __name__ == '__main__':
