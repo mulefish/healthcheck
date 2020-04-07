3f2fddad3481ae31497f95809eca77255a6a5bd5
Paul Montgomery <paul.montgomery@PRFT-11395-MAC.domain>
2020-04-07 16:12:32 -0500

    working on a check-set

--- server.py
+++ server.py
@@ -1,4 +1,7 @@
 from aiohttp import web
+import aiohttp
+import asyncio
+
 import time
 
 tokenHolder = {}
@@ -9,26 +12,36 @@ async def fetch(session, url):
     async with session.get(url) as response:
         return await response.text()
 
-async def setToken():
-    global tokenHolder
-    rightNow = time.time()
-    if rightNow - tokenHolder['when'] > 60:
+async def setToken(request):
+    rightNow = int(time.time())
+    getNew = False
+    if tokenHolder['when'] == None:
+        # New it up!
+        getNew = True
+    elif (rightNow - tokenHolder['when']) > 5.0:
+        # Get a new one!
+        getNew = True
+
+    if getNew == True:
         async with aiohttp.ClientSession() as session:
             token = await fetch(session, 'http://localhost:5000/jwttoken')
             tokenHolder['when'] = rightNow
             tokenHolder['token'] = token
-
+            print("Get a new value  {}".format(tokenHolder ))
+    else:
+        print("Use the old one {}".format(tokenHolder ))
 
 async def exec(request):
-    return web.Response(text='hello world')
+    msg = 'zoom hello world and {} when {}'.format(tokenHolder['token'], tokenHolder['when'])
+    return web.Response(text=msg)
 
 async def handle(request):
     name = request.match_info.get('name', "Anonymous")
     text = "Hello, " + name
     return web.Response(text=text)
-    return web.Response(text=text)
 
 app = web.Application()
+app.on_startup.append(setToken)
 app.add_routes([web.get('/exec', exec), 
                 web.get('/', handle),
                 web.get('/{name}', handle)])
