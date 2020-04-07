d3258902d3395b00e1af3fc34c21f89164e6291d
mulefish <finch.wren.swift@gmail.com>
2020-04-07 11:07:18 -0700

    oh - how to make a pull request w/ github?

--- test/grist_server.py
+++ test/grist_server.py
@@ -12,7 +12,7 @@ def about():
     return jsonify(result)
 
 alphanumeric = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","0","1","2","3","4","5","6","7","8","9"]
-
+ 
 @app.route('/jwttoken')
 def jwttoken(): 
     global alphanumeric
